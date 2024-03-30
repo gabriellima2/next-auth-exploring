67952086da4189b4b64bda650d102b746168fc31
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-03-30 15:22:59 -0300

    fix: available user route for admins

--- src/app/(private)/(user)/layout.tsx
+++ src/app/(private)/(user)/layout.tsx
@@ -3,15 +3,11 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 import { nextAuthConfig } from "@/modules/authentication/config/next-auth.config";
-import { UserType } from "@/commons/constants/user-type";
 
 export default async function Layout(props: PropsWithChildren) {
 	const { children } = props
 	const session = await getServerSession(nextAuthConfig)
-
 	if (!session) return redirect("/")
-	if (session.type === UserType.ADMIN) return redirect("/admin/home")
-
 	return (
 		<>
 			{children}
